77712f3f62e77261d09f71f64419052eadf626cb
rickydelorier <rdelorier@gmail.com>
2015-11-01 03:13:13 -0500

    extended can method to allow multiple permission and strict comparison

--- src/auth.js
+++ src/auth.js
@@ -1,21 +1,39 @@
 "use strict";
 
+import _ from 'lodash';
+
 class Auth {
 
     constructor(options){
         this.options = options;
+        this._permissions = options.permissions || [];
     }
 
-    can(permission){
-        return this.permissions.indexOf(permission) !== -1;
+    /**
+     * Determines if permission is in this.permission
+     *
+     * @param permission array[string]|string permissions to check for
+     * @param strict are all permissions required
+     * @returns {boolean}
+     */
+    can(permission, strict){
+
+        if(!Array.isArray(permission)){
+            return this.permissions.indexOf(permission) !== -1;
+        }
+
+        return _.intersection(
+            this.permissions,
+            permission
+        ).length >= (strict ? permission.length : 1);
     }
 
     set permissions(permissions){
-        this.options.permissions = permissions;
+        this._permissions = permissions;
     }
 
     get permissions(){
-        return this.options.permissions;
+        return this._permissions;
     }
 
 
